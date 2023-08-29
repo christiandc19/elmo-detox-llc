d9e2459a79198f1ca762929d8b82e94cd7b5c792
github_christiandc19 <christiandc19@hotmail.com>
2023-08-29 10:55:00 -0700

    styling

--- src/components/section2/Section2.js
+++ src/components/section2/Section2.js
@@ -16,19 +16,18 @@ const Section2 = () => {
 
             <div className='right'>
                   <h1>WHY CHOOSE US?</h1>
-                  <h2>"If you can quit for a day, you can quit for a lifetime.” – Benjamin Alire Sáenz</h2>
 
                   <ul>
                     <li>
                         <FaCheck className='check'/> <span> DEDICATED STAFF. </span> <br />
                         The medical professionals on our team are completely dedicated to your recovery.
-                    </li><br />
+                    </li>
                     <li>
                         <FaCheck className='check'/> <span> PROVEN METHODS. </span> <br /> Our treatment methods are solely based on proven research and science-based evidence.
-                    </li><br />
+                    </li>
                     <li>
                         <FaCheck className='check'/> <span> 24/7 SUPPORT. </span> <br /> Guidance and support are always in reach. We provide care for you whenever needed.
-                    </li><br />
+                    </li>
                   </ul>
                 </div>
         </div>
