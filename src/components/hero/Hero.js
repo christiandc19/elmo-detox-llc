ac59ee288e1a48dcecd57b1e828ec0fa95c81b69
github_christiandc19 <christiandc19@hotmail.com>
2023-09-20 18:48:05 -0700

    styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -22,16 +22,12 @@ const Hero = () => {
                         <h1>ELMO DETOX</h1>
                     <Fade left>
                         <h2>DETOX AND RESIDENTIAL TREATMENT CENTER</h2>
-
-                        {/* <h2>Substance Abuse Addiction and Mental Health Treatment Facility</h2> */}
                     </Fade>
-                        <br />
                         <div className='hero-btn'>
                         <Link to="/contact">
                         <button>Contact Us</button>
                     </Link>
                             <div>
-                            {/* <p> Your Journey to Recovery Begins Here.</p> */}
                         </div>
                     </div>
                     <br />
